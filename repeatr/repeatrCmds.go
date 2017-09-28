32e6b2654834bb70d7875534731d4577f7d0cd8a
Eric Myhre <hash@exultant.us>
2017-09-28 10:39:05 +0200

    repeatr: yank func APIs up to date.
    - Bring in all the error serialization helpers prototyped in Rio;
    - Declare fresh types for logging;
    - Fix the presense of warehouses in func args (this changed when we merged that last branch about urls now being in formulae after all);
    - Use a monitor object indirection like Rio does (makes it much easier to have DTRT zero values);
    
    ... yeah, that about sums it up.
    
    We have a missing concept for how to hand in inputs (it could be as simple as an io.Reader, but I'm not sure if we want to do that, or a channel which makes chunking control more explicit): todo.
    
    We'll need helpers for making output DTRT, because nobody really wants to deal with that event stream.  But I think that's okay: we end up needing such output buffering control proxies *anyway*, in order to deal with things like log events and user output with "\r" content not coliding to make a mess.
    
    It's not clear if this func will be able to do doubletime for repeatr's internal code concept of executors as well as the wrapper for the entire tool.  Maybe.  (But more likely is that our previous abstraction of executors was just kinda not very correctly separated... but hey, the utility of a 'demo' executor -- which does, yes, need to see the whole inputs and outputs specs -- makes things muddy.)
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- repeatr/repeatrCmds.go
+++ repeatr/repeatrCmds.go
@@ -4,9 +4,6 @@
 	The real repeatr logic implements these;
 	so do the various proxy tools (e.g. r2k8s);
 	so do some mocks, which are useful for testing.
-
-	A serial client is included in this package;
-	how to launch any remote actions is not in this package.
 */
 package repeatr
 
@@ -14,44 +11,103 @@ import (
 	"context"
 	"time"
 
+	"github.com/polydawn/go-errcat"
+
 	"go.polydawn.net/go-timeless-api"
 )
 
 type RunFunc func(
-	ctx context.Context,
-	formula *api.Formula,
-	defaultWarehouses []api.WarehouseAddr, // default input warehouses
-	outputWarehouses map[api.AbsPath][]api.WarehouseAddr, // output warehouses
-	inputWarehouses map[api.AbsPath][]api.WarehouseAddr, // input override warehouses
-	stream chan<- *Event,
+	ctx context.Context, // Long-running call.  Cancellable.
+	formula api.Formula, // What formula to run.  Comes complete with fetch and save paths.
+	// TODO actually do need input support here
+	monitor Monitor, // Optionally: callbacks for progress monitoring.  Also where stdout/stderr is gathered.
 ) (*api.RunRecord, error)
 
 /*
-	A "union" type of all the kinds of event that may be generated in the
-	course of running a formula.
-
-	(RunRecords aren't forwarded through 'stream' chans like this, but they're
-	defined in the union because it's about where they're sent on the wire.)
+	Monitoring configuration structs, and message types used.
 */
-type Event struct {
-	Log       *LogItem       `refmt:"log,omitempty"`       // Log events are repeatr's logs.
-	Journal   string         `refmt:"journal,omitempty"`   // User output strings are called journal events.
-	RunRecord *api.RunRecord `refmt:"runRecord,omitempty"` // RunRecords are the final event emitted by a run.
-}
+type (
+	Monitor struct {
+		Chan chan<- Event
+	}
+
+	// A "union" type of all the kinds of event that may be generated in the
+	// course of any of the functions.
+	//
+	// (The 'Result' message seen on the wire, but converted into returns;
+	// it is never sent to the Monitor.Chan.)
+	Event struct {
+		Log    Event_Log    `refmt:"log,omitempty"`
+		Output Event_Output `refmt:"txt,omitempty"`
+		Result Event_Result `refmt:"result,omitempty"`
+	}
+
+	// Logs from repeatr code.
+	Event_Log struct {
+		Time  time.Time   `refmt:"t"`
+		Level int         `refmt:"lvl"`
+		Msg   string      `refmt:"msg"`
+		Ctx   [][2]string `refmt:"ctx,omitempty"`
+	}
+
+	// Output from the contained process (stdout/stderr conjoined).
+	// Stderr/stdout are conjoined so their ordering does not slip.
+	// There is no guarantee of buffering (especially not line buffering);
+	// in other words, `printch('.')` may indeed flush.
+	Event_Output struct {
+		Time time.Time `refmt:"t"`
+		Msg  string    `refmt:"msg"`
+	}
+
+	// Final results.  (Converted into returns; not sent to Monitor.)
+	Event_Result struct {
+		RunRecord api.RunRecord `refmt:",omityEmpty"`
+		Error     *Error        `refmt:",omityEmpty"`
+	}
+)
 
 /*
-	Log events from repeatr as serialized.
+	`errcat.Error` implementor with `repeatr.ErrorCategory` concrete category.
+
+	This is necessary for deserialization in client APIs to yield correctly typed categories.
+	Note that it does not enforce a check that the error category is from the package's
+	enumerated constants when deserializing.
 */
-type LogItem struct {
-	Time  time.Time         `refmt:"t"`
-	Level int               `refmt:"lvl"`
-	Msg   string            `refmt:"msg"`
-	Ctx   map[string]string `refmt:"ctx,omitempty"`
+type Error struct {
+	Category_ ErrorCategory     `json:"category"          refmt:"category"`
+	Message_  string            `json:"message"           refmt:"message"`
+	Details_  map[string]string `json:"details,omitempty" refmt:"details,omitempty"`
+}
+
+func (e *Error) Category() interface{}      { return e.Category_ }
+func (e *Error) Message() string            { return e.Message_ }
+func (e *Error) Details() map[string]string { return e.Details_ }
+func (e *Error) Error() string              { return e.Message_ }
+
+/*
+	Helper to set the Error field of the result message structure,
+	handling type conversion checks.
+*/
+func (r *Event_Result) SetError(err error) {
+	if err == nil {
+		r.Error = nil
+		return
+	}
+	r.Error = &Error{}
+	if e2, ok := err.(errcat.Error); ok {
+		r.Error.Category_ = errcat.Category(err).(ErrorCategory)
+		r.Error.Message_ = e2.Message()
+		r.Error.Details_ = e2.Details()
+	} else {
+		r.Error.Category_ = ErrRPCBreakdown // :/
+		r.Error.Message_ = err.Error()
+	}
 }
 
 type ErrorCategory string
 
 const (
-	ErrUsage    ErrorCategory = "repeatr-usage-error"    // Indicates some piece of user input to a command was invalid and unrunnable.
-	ErrExecutor ErrorCategory = "repeatr-executor-error" // Indicates an executor failed to finish (or possibly even start) executing a formula's action.
+	ErrUsage        = ErrorCategory("repeatr-usage-error")    // Indicates some piece of user input to a command was invalid and unrunnable.
+	ErrExecutor     = ErrorCategory("repeatr-executor-error") // Indicates an executor failed to finish (or possibly even start) executing a formula's action.
+	ErrRPCBreakdown = ErrorCategory("repeatr-rpc-breakdown")  // Raised when running a remote process and the control channel is lost, the process fails to start, or unrecognized messages are received.
 )
