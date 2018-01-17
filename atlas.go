b9b14b2bb662e9c1bc99132ad3dc868c5b571381
Eric Myhre <hash@exultant.us>
2018-01-17 18:41:49 +0100

    ReleaseItemID is now in the *repeatr* atlas set.
    This is kind of semantically dubious -- see the entire commit message where
    the Imports field was added to FormulaUnion -- but since we do refer to
    ReleaseItemID via the Imports field, in this atlas it now must be.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- atlas.go
+++ atlas.go
@@ -12,6 +12,7 @@ var rioAtlasEntries = []*atlas.AtlasEntry{
 var repeatrAtlasEntries = []*atlas.AtlasEntry{
 	FormulaUnion_AtlasEntry,
 	Formula_AtlasEntry,
+	ReleaseItemID_AtlasEntry,
 	FormulaContext_AtlasEntry,
 	FormulaAction_AtlasEntry,
 	FormulaUserinfo_AtlasEntry,
@@ -28,7 +29,6 @@ var formulaCasAtlasEntries = []*atlas.AtlasEntry{
 
 var hitchAtlasEntries = []*atlas.AtlasEntry{
 	Catalog_AtlasEntry,
-	ReleaseItemID_AtlasEntry,
 	ReleaseEntry_AtlasEntry,
 	Replay_AtlasEntry, // probably delete now
 	Step_AtlasEntry,   // probably delete now
