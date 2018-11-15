299a504071060e9ab0976b2425361859caa4142a
Eric Myhre <hash@exultant.us>
2018-11-15 13:44:42 -0600

    Drop MainModule and its ImportsPinned.
    This was an idea from earlier drafts of the redux branch, but has never
    been used.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- module.go
+++ module.go
@@ -1,9 +1,5 @@
 package api
 
-type MainModule struct {
-	ImportsPinned map[SubmoduleSlotRef]WareID // only allowed on top module (since it contains info for all submodules as well).
-	Module
-}
 type Module struct {
 	Imports map[SlotName]ImportRef
 	Steps   map[StepName]StepUnion
